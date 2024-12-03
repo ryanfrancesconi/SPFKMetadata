6e6462cc0583b62ebed87ce2867f499af8a9047c
Ryan Francesconi <thiscomputermagic@gmail.com>
2024-12-03 12:13:35 -0800

    1871, Global Key Change Add better queue management to RegionRendererQueue

--- Tests/SPFKMetadataTests/TagFileTests.swift
+++ Tests/SPFKMetadataTests/TagFileTests.swift
@@ -11,4 +11,11 @@ class TagFileTests: SPFKMetadataTestModel {
 
         #expect(tagFile.dictionary?["TITLE"] as? String == "ID3: 12345678910 mono 48k")
     }
+    
+    
+    @Test func testParseID3_2() async throws {
+        let tagFile = try #require(TagFile(path: "/Users/rf/Downloads/sounds/packs/app.add.soundpack.drones_vol_2_wav/DRN02_Bed_Drone_Synthetic_Wow_Key E.mp3"))
+
+        Swift.print(tagFile.dictionary)
+    }
 }
