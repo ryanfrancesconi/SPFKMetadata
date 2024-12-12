e9e34b840fd01940637451fe043c6fb8d550ae2e
Ryan Francesconi <thiscomputermagic@gmail.com>
2024-12-12 13:23:58 -0800

    fix, remove local paths from test

--- Tests/SPFKMetadataTests/TagLibBridgeTests.swift
+++ Tests/SPFKMetadataTests/TagLibBridgeTests.swift
@@ -13,7 +13,6 @@ class TagLibBridgeTests: SPFKMetadataTestModel {
     @Test func readWriteTagProperties() async throws {
         let tmpfile = try copy(to: bin, url: wav_bext_v2)
         var dict = try #require(TagLibBridge.getProperties(tmpfile.path) as? [String: String])
-
         #expect(dict["TITLE"] == "Stonehenge")
 
         // set
@@ -39,11 +38,14 @@ class TagLibBridgeTests: SPFKMetadataTestModel {
     }
 
     @Test func copyMetadata() async throws {
-        let source = URL(fileURLWithPath: "/Users/rf/Desktop/tag/id3.mp3")
-        let destination = URL(fileURLWithPath: "/Volumes/ADD2/mp3/Spinal Tap/This Is Spinal Tap/and-oh-how-they-danced.mp3")
+        let source = mp3_id3
+        let destination = tabla_mp4
         let tmpfile = try copy(to: bin, url: destination)
 
         let success = TagLibBridge.copyTags(fromPath: source.path, toPath: tmpfile.path)
         #expect(success)
+        
+        let dict = try #require(TagLibBridge.getProperties(tmpfile.path) as? [String: String])
+        #expect(dict["TITLE"] == "Stonehenge")
     }
 }
