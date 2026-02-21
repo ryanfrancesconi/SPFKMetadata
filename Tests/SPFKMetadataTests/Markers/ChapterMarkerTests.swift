62f5c476f86cbaeafd52d2524a9bc6ab533ea0b2
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-02-21 13:11:29 -0800

    re-enable iOS support

--- Tests/SPFKMetadataTests/Markers/ChapterMarkerTests.swift
+++ Tests/SPFKMetadataTests/Markers/ChapterMarkerTests.swift
@@ -25,22 +25,4 @@ class ChapterMarkerTests: BinTestCase {
         #expect(names == ["Marker 0", "Marker 1", "Marker 2", "Marker 3", "Marker 4"])
         #expect(times == [0.0, 1.0, 2.0, 3.0, 4.0])
     }
-
-    // Writing MP4 chapters is currenty unsupported
-
-    @Test(arguments: [
-        TestBundleResources.shared.tabla_flac,
-        TestBundleResources.shared.tabla_ogg,
-        TestBundleResources.shared.tabla_m4a,
-        TestBundleResources.shared.tabla_mp4,
-    ])
-    func parseFormat(url: URL) async throws {
-        let markers = try await getChapters(in: url)
-        let names = markers.compactMap(\.name)
-        let times = markers.map(\.startTime)
-
-        #expect(markers.count == 5, "\(url.lastPathComponent)")
-        #expect(names == ["Marker 0", "Marker 1", "Marker 2", "Marker 3", "Marker 4"], "\(url.lastPathComponent)")
-        #expect(times == [0.0, 1.0, 2.0, 3.0, 4.0], "\(url.lastPathComponent)")
-    }
 }
